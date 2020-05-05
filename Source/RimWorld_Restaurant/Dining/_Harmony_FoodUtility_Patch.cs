b7925429e89293582725ff0d4729d220372b10cd
Rando Wiltschek <randomw@gmx.net>
2020-05-05 14:42:43 +0200

    Created patch for finding dining spots

---
+++ Source/RimWorld_Restaurant/Dining/_Harmony_FoodUtility_Patch.cs
@@ -0,0 +1,44 @@
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Restaurant.Dining
+{
+    /// <summary>
+    /// For allowing pawns to find DiningSpots when hungry
+    /// </summary>
+    internal static class _Harmony_FoodUtility_Patch
+    {
+        [HarmonyPatch(typeof(FoodUtility), "TryFindBestFoodSourceFor")]
+        public class TryFindBestFoodSourceFor
+        {
+            [HarmonyPrefix]
+            internal static bool Prefix(Pawn getter, Pawn eater, ref bool __result, ref Thing foodSource, ref Thing foodDef)
+            {
+                if (getter != eater)
+                {
+                    Log.Message($"{getter?.NameShortColored} != {eater?.NameShortColored}.");
+                    return true; // Run original code
+                }
+
+                bool canManipulate = getter.RaceProps.ToolUser && getter.health.capacities.CapableOf(PawnCapacityDefOf.Manipulation);
+                if (!canManipulate)
+                {
+                    return true; // Run original code
+                }
+
+                bool allowDrug = !eater.IsTeetotaler();
+                var diningSpot = DiningUtility.FindDiningSpotFor(getter, out foodDef);
+
+                if (diningSpot != null)
+                {
+                    foodSource = diningSpot;
+                    __result = true;
+                    return false; // Don't run original code
+                }
+
+                return true; // Run original code
+            }
+        }
+    }
+}
