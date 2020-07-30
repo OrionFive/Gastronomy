27a3aecc76c54184504cc09a91580351271c9ad0
OrionFive <randomw@gmx.net>
2020-07-30 10:03:10 +0200

    Made initial food menu selection smarter

--- Source/RimWorld_Restaurant/Generic/RestaurantMenu.cs
+++ Source/RimWorld_Restaurant/Generic/RestaurantMenu.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using RimWorld;
 using Verse;
 
@@ -15,28 +16,22 @@ namespace Restaurant
 
         public bool IsOnMenu(ThingDef def)
         {
-            if (menuFilter == null) InitMenuFilter();
+            if (menuFilter == null) InitMenuFilter(menuGlobalFilter);
             return menuFilter.Allows(def);
         }
 
         public bool IsOnMenu(Thing thing)
         {
-            if (menuFilter == null) InitMenuFilter();
+            if (menuFilter == null) InitMenuFilter(menuGlobalFilter);
             return menuFilter.Allows(thing);
         }
 
         public void GetMenuFilters(out ThingFilter filter, out ThingFilter globalFilter)
         {
-            if (menuFilter == null) InitMenuFilter();
-            filter = menuFilter;
             if (menuGlobalFilter == null) InitMenuGlobalFilter();
             globalFilter = menuGlobalFilter;
-        }
-
-        private void InitMenuFilter()
-        {
-            menuFilter = new ThingFilter();
-            menuFilter.SetAllowAll(menuGlobalFilter);
+            if (menuFilter == null) InitMenuFilter(globalFilter);
+            filter = menuFilter;
         }
 
         private void InitMenuGlobalFilter()
@@ -45,6 +40,25 @@ namespace Restaurant
             menuGlobalFilter.SetAllow(ThingCategoryDefOf.Foods, true);
             menuGlobalFilter.SetAllow(ThingCategoryDefOf.Drugs, true);
             menuGlobalFilter.allowedQualitiesConfigurable = true;
+        }
+
+        private void InitMenuFilter(ThingFilter globalFilter)
+        {
+            menuFilter = new ThingFilter();
+            menuFilter.SetAllowAll(globalFilter);
+            foreach (var def in menuFilter.AllowedThingDefs.ToArray())
+            {
+                if(def.ingestible?.joy <= 0)
+                {
+                    var preferability = def.ingestible.preferability;
+                    if (preferability == FoodPreferability.DesperateOnly 
+                        || preferability == FoodPreferability.DesperateOnlyForHumanlikes 
+                        || preferability == FoodPreferability.RawBad)
+                        menuFilter.SetAllow(def, false);
+
+                    if (preferability == FoodPreferability.Undefined) Log.Message($"{def.LabelCap} has preferability undefined");
+                }
+            }
         }
     }
 }
