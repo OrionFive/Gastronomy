7fe7883839bb848d5ffa88c76d70ea94817fb679
OrionFive <randomw@gmx.net>
2020-07-30 10:02:44 +0200

    Added cache for food optimality

--- Source/RimWorld_Restaurant/Generic/RestaurantStock.cs
+++ Source/RimWorld_Restaurant/Generic/RestaurantStock.cs
@@ -10,8 +10,16 @@ namespace Restaurant
 {
     public class RestaurantStock : IExposable
     {
-        [NotNull] private readonly List<Thing> stock = new List<Thing>();
-        [NotNull] public IEnumerable<Thing> AllStock => stock.AsReadOnly();
+        private class FoodOptimality
+        {
+            public Pawn pawn;
+            public ThingDef def;
+            public float value;
+        }
+
+        [NotNull] private readonly List<Thing> stockCache = new List<Thing>();
+        [NotNull] private readonly List<FoodOptimality> optimalityCache = new List<FoodOptimality>();
+        [NotNull] public IEnumerable<Thing> AllStock => stockCache.AsReadOnly();
         [NotNull] private Map Map => Restaurant.map;
         [NotNull] private RestaurantMenu Menu => Restaurant.Menu;
         [NotNull] private RestaurantController Restaurant { get; }
@@ -26,29 +34,35 @@ namespace Restaurant
         public bool HasAnyFoodFor([NotNull] Pawn pawn, bool allowDrug)
         {
             //Log.Message($"{pawn.NameShortColored}: HasFoodFor: Defs: {stock.Select(item=>item.def).Count(s => WillConsume(pawn, allowDrug, s))}");
-            return stock.Select(item => item.def).Any(s => WillConsume(pawn, allowDrug, s));
+            return stockCache.Select(item => item.def).Any(s => WillConsume(pawn, allowDrug, s));
         }
 
         public ThingDef GetBestFoodTypeFor([NotNull] Pawn pawn, bool allowDrug)
         {
-            var best = stock.Select(item => item.def).Distinct().Where(def => WillConsume(pawn, allowDrug, def)).MaxBy(def => FoodOptimality(pawn, def));
+            var best = stockCache.Select(item => item.def).Distinct().Where(def => WillConsume(pawn, allowDrug, def)).MaxBy(def => GetFoodOptimality(pawn, def));
             //Log.Message($"{pawn.NameShortColored}: GetBestFoodFor: {best?.label}");
             return best;
         }
 
         public ThingDef GetRandomFoodTypeFor([NotNull] Pawn pawn, bool allowDrug)
         {
-            var random = stock.Select(item => item.def).Distinct().Where(def => WillConsume(pawn, allowDrug, def)).RandomElementByWeight(def => FoodOptimality(pawn, def));
+            var random = stockCache.Select(item => item.def).Distinct().Where(def => WillConsume(pawn, allowDrug, def)).RandomElementByWeight(def => GetFoodOptimality(pawn, def));
             //Log.Message($"{pawn.NameShortColored}: GetBestFoodFor: {best?.label}");
             return random;
         }
 
-        private float FoodOptimality(Pawn pawn, ThingDef def)
+        private float GetFoodOptimality(Pawn pawn, ThingDef def)
         {
             // Optimality can be negative
-            Log.Message($"{pawn.NameShortColored} - {def.LabelCap}");
-            var dummyFoodSource = stock[0]; // Can be null again once erdelf fixes the patch
-            return Mathf.Max(0, FoodUtility.FoodOptimality(pawn, dummyFoodSource, def, 0));
+            var dummyFoodSource = stockCache[0]; // Can be null again once erdelf fixes the patch
+            var optimality = optimalityCache.FirstOrDefault(o => o.pawn == pawn && o.def == def);
+            if (optimality == null)
+            {
+                optimality = new FoodOptimality {pawn = pawn, def = def, value = Mathf.Max(0, FoodUtility.FoodOptimality(pawn, dummyFoodSource, def, 0))};
+                optimalityCache.Add(optimality);
+            }
+
+            return optimality.value;
         }
 
         private static bool WillConsume(Pawn pawn, bool allowDrug, ThingDef s)
@@ -58,16 +72,20 @@ namespace Restaurant
 
         public Thing GetServableThing(Order order, Pawn pawn)
         {
-            return stock.Where(o => o.Spawned && o.def == order.consumableDef)
-                .OrderBy(o => pawn.Position.DistanceToSquared(o.Position))
-                .FirstOrDefault(o => pawn.CanReserveAndReach(o, PathEndMode.Touch, Danger.None, o.stackCount, 1));
+            return stockCache.Where(o => o.Spawned && o.def == order.consumableDef).OrderBy(o => pawn.Position.DistanceToSquared(o.Position)).FirstOrDefault(o => pawn.CanReserveAndReach(o, PathEndMode.Touch, Danger.None, o.stackCount, 1));
         }
 
         public void RareTick()
         {
             // Refresh entire stock
-            stock.Clear();
-            stock.AddRange(Map.listerThings.ThingsInGroup(ThingRequestGroup.FoodSource).Where(t => t.def.IsIngestible && Menu.IsOnMenu(t)));
+            stockCache.Clear();
+            stockCache.AddRange(Map.listerThings.ThingsInGroup(ThingRequestGroup.FoodSource).Where(t => t.def.IsIngestible && !t.def.IsCorpse && Menu.IsOnMenu(t)));
+
+            // Slowly empty cache again
+            if (optimalityCache.Count > 0)
+            {
+                optimalityCache.RemoveAt(0);
+            }
         }
     }
 }
