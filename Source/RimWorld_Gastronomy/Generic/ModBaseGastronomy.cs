f4ea2d4a2cabd9688c5dc8e0155ce806c580d4e3
OrionFive <wiltschek@gmx.net>
2021-05-27 17:49:19 +0200

    Added message that Cash Register is required; added dll so game starts without error

--- Source/RimWorld_Gastronomy/Generic/ModBaseGastronomy.cs
+++ Source/RimWorld_Gastronomy/Generic/ModBaseGastronomy.cs
@@ -1,3 +1,6 @@
+using System;
+using CashRegister;
+using Gastronomy.Restaurant;
 using HugsLib;
 using Verse;
 
@@ -13,6 +16,12 @@ namespace Gastronomy
         public override void DefsLoaded()
         {
             settings = new Settings(Settings);
+
+            if (!RegisterUtility.TryAddTab<ITab_Register_Restaurant>())
+            {
+                GenUI.ErrorDialog(
+                    "Gastronomy now requires the 'Cash Register' mod to be loaded.\n\nI had to create it in order to be able to work on a new feature. Sorry about this inconvenience.\n\nIf you add the 'Cash Register' mod, Gastronomy will continue working as before.\n\n\n-- Regards, Orion");
+            }
         }
     }
 }
