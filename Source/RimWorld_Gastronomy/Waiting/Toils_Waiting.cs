112a80203d37215a7a7245aa58459e080600c4e0
Rando Wiltschek <wiltschek@gmx.net>
2024-05-04 12:15:22 +0200

    - Fix: Waiters not standing near register when idle

--- Source/RimWorld_Gastronomy/Waiting/Toils_Waiting.cs
+++ Source/RimWorld_Gastronomy/Waiting/Toils_Waiting.cs
@@ -171,16 +171,16 @@ namespace Gastronomy.Waiting
                             return c.Standable(actor.Map) && c.GetFirstPawn(actor.Map) == null;
                         }
 
-                        if (CellFinder.TryFindRandomReachableNearbyCell(actor.Position, actor.Map, radius, TraverseParms.For(TraverseMode.NoPassClosedDoors), Validator, null, out var result))
+                        if (CellFinder.TryFindRandomReachableNearbyCell(target.Cell, actor.Map, radius, TraverseParms.For(TraverseMode.NoPassClosedDoors), Validator, null, out var result))
                         {
                             curJob.SetTarget(cellInd, result);
-                            Log.Message($"{actor.NameShortColored} found a place to stand at {result}. radius = {radius}");
+                            // Log.Message($"{actor.NameShortColored} found a place to stand at {result}. radius = {radius}");
                             return;
                         }
                     }
 
                     // This can happen if there's no space or it's crowded
-                    Log.Error(actor + " could not find standable cell adjacent to " + target);
+                    // Log.Error(actor + " could not find standable cell adjacent to " + target);
                     actor.jobs.curDriver.EndJobWith(JobCondition.Incompletable);
                 }
             };
